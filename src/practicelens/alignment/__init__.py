88b60a033bb52b237bcc2887d96fd042b86a01e8
Kymuco <ikymuco@gmail.com>
2026-04-03 18:54:51 +0600

    feat(alignment): add reference-aware DTW frame alignment

---
+++ src/practicelens/alignment/__init__.py
@@ -0,0 +1,6 @@
+"""Feature alignment helpers."""
+
+from practicelens.alignment.dtw import align_feature_bundles
+from practicelens.alignment.models import AlignmentPair, AlignmentPath
+
+__all__ = ["AlignmentPair", "AlignmentPath", "align_feature_bundles"]
