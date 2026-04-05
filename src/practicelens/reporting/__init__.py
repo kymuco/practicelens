d12d00b314e34020f305aa992e3855606a4f0ef4
Kymuco <ikymuco@gmail.com>
2026-04-05 16:25:14 +0600

    fix(ci): stabilize test discovery and reporting exports

--- src/practicelens/reporting/__init__.py
+++ src/practicelens/reporting/__init__.py
@@ -4,3 +4,11 @@ from practicelens.reporting.csv_report import report_to_csv_text
 from practicelens.reporting.json_report import report_to_json_payload, report_to_json_text
 from practicelens.reporting.markdown_report import report_to_markdown
 from practicelens.reporting.svg_report import report_to_svg
+
+__all__ = [
+    "report_to_csv_text",
+    "report_to_json_payload",
+    "report_to_json_text",
+    "report_to_markdown",
+    "report_to_svg",
+]
