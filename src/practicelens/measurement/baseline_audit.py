af40c13a1320a7a83d6c1efd4a35db97743f0d04
Ikymuco <ikymuco@gmail.com>
2026-09-24 17:06:23 +0600

    style(measurement): make adjacent zip non-strict explicitly

--- src/practicelens/measurement/baseline_audit.py
+++ src/practicelens/measurement/baseline_audit.py
@@ -430,7 +430,7 @@ def _directional_sensitivity_verdict(values: list[float]) -> AuditVerdict:
 def _nonincreasing_monotonicity_verdict(values: list[float]) -> AuditVerdict:
     has_decrease = False
     has_increase = False
-    for left, right in zip(values, values[1:]):
+    for left, right in zip(values, values[1:], strict=False):
         delta = right - left
         if delta < -STRICT_DELTA_EPSILON:
             has_decrease = True
