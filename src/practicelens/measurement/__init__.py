1cb1a6d92ada295b989cf41c26c32a434f82a510
Ikymuco <ikymuco@gmail.com>
2026-09-24 16:58:14 +0600

    refactor(measurement): keep controlled harness off public contract surface

--- src/practicelens/measurement/__init__.py
+++ src/practicelens/measurement/__init__.py
@@ -13,21 +13,8 @@ from practicelens.measurement.contracts import (
     measurement_experiment_record_to_json,
     measurement_experiment_record_to_payload,
 )
-from practicelens.measurement.controlled_perturbations import (
-    CONTROLLED_PERTURBATION_GENERATOR_VERSION,
-    CONTROLLED_PERTURBATION_SCHEMA_VERSION,
-    ControlledPerturbationCase,
-    ControlledPerturbationHarnessResult,
-    controlled_perturbation_manifest_payload,
-    generate_controlled_perturbation_harness,
-)
-
 
 __all__ = [
-    "CONTROLLED_PERTURBATION_GENERATOR_VERSION",
-    "CONTROLLED_PERTURBATION_SCHEMA_VERSION",
-    "ControlledPerturbationCase",
-    "ControlledPerturbationHarnessResult",
     "MEASUREMENT_EXPERIMENT_SCHEMA_VERSION",
     "AnalysisConfigSnapshot",
     "CandidateMeasurementResult",
@@ -37,8 +24,6 @@ __all__ = [
     "InterventionSpec",
     "MeasurementExperimentRecord",
     "MeasurementRunProvenance",
-    "controlled_perturbation_manifest_payload",
-    "generate_controlled_perturbation_harness",
     "measurement_experiment_record_to_json",
     "measurement_experiment_record_to_payload",
 ]
