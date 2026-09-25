a6964a89f3427931e350fc2e119fcbaaedbb4845
Ikymuco <ikymuco@gmail.com>
2026-09-25 14:09:54 +0600

    fix(preprocessing): export fixed-padding trim

--- src/practicelens/preprocessing/__init__.py
+++ src/practicelens/preprocessing/__init__.py
@@ -2,6 +2,11 @@
 
 from practicelens.preprocessing.normalize import peak_normalize
 from practicelens.preprocessing.resample import resample_linear
-from practicelens.preprocessing.trim import trim_silence
+from practicelens.preprocessing.trim import trim_silence, trim_silence_fixed_padding
 
-__all__ = ["peak_normalize", "resample_linear", "trim_silence"]
+__all__ = [
+    "peak_normalize",
+    "resample_linear",
+    "trim_silence",
+    "trim_silence_fixed_padding",
+]
