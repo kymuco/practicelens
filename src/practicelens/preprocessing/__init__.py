cecf80c75b5fd7e4a35d4e5d6c83b5e70b3d6dd7
Ikymuco <ikymuco@gmail.com>
2026-09-25 15:08:18 +0600

    refactor(preprocessing): expose only admitted preprocessing primitives

--- src/practicelens/preprocessing/__init__.py
+++ src/practicelens/preprocessing/__init__.py
@@ -2,12 +2,6 @@
 
 from practicelens.preprocessing.normalize import peak_normalize, remove_dc_offset
 from practicelens.preprocessing.resample import resample_linear
-from practicelens.preprocessing.trim import trim_silence, trim_silence_fixed_padding
+from practicelens.preprocessing.trim import trim_silence
 
-__all__ = [
-    "peak_normalize",
-    "remove_dc_offset",
-    "resample_linear",
-    "trim_silence",
-    "trim_silence_fixed_padding",
-]
+__all__ = ["peak_normalize", "remove_dc_offset", "resample_linear", "trim_silence"]
