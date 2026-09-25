60633941fae235b32eb68f2525c9bd9967ae32ce
Ikymuco <ikymuco@gmail.com>
2026-09-25 14:15:50 +0600

    fix(preprocessing): export DC offset removal

--- src/practicelens/preprocessing/__init__.py
+++ src/practicelens/preprocessing/__init__.py
@@ -1,11 +1,12 @@
 """Lightweight audio preprocessing helpers."""
 
-from practicelens.preprocessing.normalize import peak_normalize
+from practicelens.preprocessing.normalize import peak_normalize, remove_dc_offset
 from practicelens.preprocessing.resample import resample_linear
 from practicelens.preprocessing.trim import trim_silence, trim_silence_fixed_padding
 
 __all__ = [
     "peak_normalize",
+    "remove_dc_offset",
     "resample_linear",
     "trim_silence",
     "trim_silence_fixed_padding",
